538687344b051615a1377e8803c84c88219eb750
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2024-01-28 16:05:05 +0300

    Update footer links

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,13 +5,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by">
-      Developed by <b><a href="mailto:xpirixii@gmail.com" target="_blank">xpirix</a></b><br>
       Support this project on <b><a href="https://github.com/geosanitation/frontend" target="_blank">github</a>.</b><br>
     </span>
     <div class="socials">
-      <a href="https://github.com/Xpirix" target="_blank" class="ion ion-social-github"></a>
-      <a href="https://www.linkedin.com/in/xpirix" target="_blank" class="ion ion-social-linkedin"></a>
-      <a href="https://twitter.com/Xpirix3" target="_blank" class="ion ion-social-twitter"></a>
+      <a href="https://github.com/geosanitation" target="_blank" class="ion ion-social-github"></a>
     </div>
   `,
 })
